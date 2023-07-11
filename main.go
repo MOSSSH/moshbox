545c7c3cec49c7d2faa084ef31ee462e0280d6cf
MOSSSH <83137708+MOSSSH@users.noreply.github.com>
2023-07-11 23:06:52 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("Please enter your name.")
+	var name string
+	fmt.Scanln(&name)
+	fmt.Printf("Hi, %s! I'm Go!", name)
+	name = strings.TrimSpace(name)
 }
